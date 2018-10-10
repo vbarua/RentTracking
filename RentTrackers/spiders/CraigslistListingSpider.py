d908ddee375770fbc9ac231bb304be547f0c935e
Victor Barua <victor.barua@gmail.com>
2018-10-09 23:56:55 -0700

    Extracting CraiglistParsingUtilities

--- RentTrackers/spiders/CraigslistListingSpider.py
+++ RentTrackers/spiders/CraigslistListingSpider.py
@@ -1,129 +1,10 @@
 import os
-import re
-import locale
 import scrapy
 
 from RentTrackers.managers.LoggerManager import LoggerManager as logger
-from RentTrackers.models.HouseUnit import HouseUnit
-from RentTrackers.models.LatLng import LatLng
-from RentTrackers.models.Rental import Rental
+import RentTrackers.spiders.CraigslistParsingUtilities as cpu
 
 log_tag = "Craigslist"
-
-
-def extract_area(s):
-    """
-    Extract area from string like: '1br 1ba 600'. Last number is area.
-    
-    :param s: the string to parse
-    :return: an extraction of the square footage, if found
-    """
-    match = re.search(r'\d+$', s)
-    if match:
-        return match.group()
-    else:
-        return ""
-
-
-def extract_bedrooms(s):
-    """
-    Extract # of bathrooms from a string like: '1br 1ba 600'
-    
-    :param s: the string to parse
-    :return: an extraction of the number of bedrooms, if found
-     """
-    match = re.search(r'\d+br', s)
-    if match:
-        bedrooms = match.group()[:-2]
-        return bedrooms
-    else:
-        return ""
-
-
-def extract_bathrooms(s):
-    """
-    Extract # of bedrooms from a string like: '1br 1ba 600'
-    
-    :param s: the string to parse
-    :return: an extraction of the number of bathrooms, if found
-    """
-    match = re.search(r'\d+ba', s)
-    if match:
-        bathrooms = match.group()[:-2]
-        return bathrooms
-    else:
-        return ""
-
-
-def extract_price(response):
-    """
-    Extract price 
-
-    :param response: the web response to parse
-    :return: an extraction of the price (trimmed to an integer)
-    """
-    price = response.css('span.price::text').extract_first()
-    decimal_point_char = locale.localeconv()['decimal_point']
-    return re.sub(r'[^0-9' + decimal_point_char + r']+', '', price)
-
-
-def extract_address(response):
-    """
-
-    :param response: 
-    :return: 
-    """
-    address_option = response.css("span.postingtitletext small::text").extract_first()
-    # Address are formatted as " (ADDRESS)"
-    if address_option is None:
-        return ""
-    else:
-        return address_option.lstrip(" (").rstrip(' )')
-
-
-def extract_bdrs_bths_area(response):
-    """
-
-    :param response: 
-    :return: 
-    """
-    raw_bdrs_bths_area = response.css("p.attrgroup span.shared-line-bubble b::text").extract()
-    bdrs_bths_area = " ".join(raw_bdrs_bths_area).lower()
-
-    area = extract_area(bdrs_bths_area)
-    num_bathrooms = extract_bathrooms(bdrs_bths_area)
-    num_bedrooms = extract_bedrooms(bdrs_bths_area)
-    return (num_bedrooms, num_bathrooms, area)
-
-
-def extract_housing_type(attributes):
-    """
-    Return the type of housing if available.
-
-    :param attributes: 
-    :return: 
-    """
-    return "TODO"
-
-
-def extract_laundry_type(attributes):
-    """
-    Return the type of laundry.
-
-    :param attributes: 
-    :return: 
-    """
-    return False
-
-
-def extract_parking_type(attributes):
-    """
-    Return the type of parking if available.
-
-    :param attributes: 
-    :return: 
-    """
-    return 0
 
 
 def get_samples_directory():
@@ -187,7 +68,7 @@ class CraigslistListingSpider(scrapy.Spider):
 
     def parse_search_page(self, response):
         """
-        Parses the contents of a Craigslist search page. Yields a series of Craislist list page Requests.
+        Parses the contents of a Craigslist search page. Yields a series of Craigslist list page Requests.
         :param response: an instance of scrapy.http.response.Response
         :return: Yields a series of scrapy.http.request.Request
         """
@@ -213,24 +94,24 @@ class CraigslistListingSpider(scrapy.Spider):
         post_link = response.css("link::attr(href)").extract_first()
         post_id = post_link.split("/")[-1]
         post_time = response.css("time::attr(datetime)").extract_first()
-        price = extract_price(response)
+        price = cpu.extract_price(response)
 
         # LatLng
         latitude = response.css("#map::attr(data-latitude)").extract_first()
         longitude = response.css("#map::attr(data-longitude)").extract_first()
 
         # HouseInfo
-        address = extract_address(response)
-        (num_bedrooms, num_bathrooms, area) = extract_bdrs_bths_area(response)
+        address = cpu.extract_address(response)
+        (num_bedrooms, num_bathrooms, area) = cpu.extract_bdrs_bths_area(response)
 
         attributes = " ".join(response.css("p.attrgroup span::text").extract()).lower()
         cats_allowed = "cats are ok" in attributes
         dogs_allowed = "dogs are ok" in attributes
         is_furnished = "TODO"
-        laundry_type = extract_laundry_type(attributes)
-        housing_type = extract_housing_type(attributes)
+        laundry_type = cpu.extract_laundry_type(attributes)
+        housing_type = cpu.extract_housing_type(attributes)
         no_smoking = "no smoking" in attributes
-        parking_type = extract_parking_type(attributes)
+        parking_type = cpu.extract_parking_type(attributes)
         wheelchair_accessible = "wheelchair accessible" in attributes
 
         # TODO: Use models
---
+++ RentTrackers/spiders/CraigslistParsingUtilities.py
@@ -0,0 +1,117 @@
+import locale
+import re
+
+
+def extract_area(s):
+    """
+    Extract area from string like: '1br 1ba 600'. Last number is area.
+
+    :param s: the string to parse
+    :return: an extraction of the square footage, if found
+    """
+    match = re.search(r'\d+$', s)
+    if match:
+        return match.group()
+    else:
+        return ""
+
+
+def extract_bedrooms(s):
+    """
+    Extract # of bathrooms from a string like: '1br 1ba 600'
+
+    :param s: the string to parse
+    :return: an extraction of the number of bedrooms, if found
+     """
+    match = re.search(r'\d+br', s)
+    if match:
+        bedrooms = match.group()[:-2]
+        return bedrooms
+    else:
+        return ""
+
+
+def extract_bathrooms(s):
+    """
+    Extract # of bedrooms from a string like: '1br 1ba 600'
+
+    :param s: the string to parse
+    :return: an extraction of the number of bathrooms, if found
+    """
+    match = re.search(r'\d+ba', s)
+    if match:
+        bathrooms = match.group()[:-2]
+        return bathrooms
+    else:
+        return ""
+
+
+def extract_price(response):
+    """
+    Extract price
+
+    :param response: the web response to parse
+    :return: an extraction of the price (trimmed to an integer)
+    """
+    price = response.css('span.price::text').extract_first()
+    decimal_point_char = locale.localeconv()['decimal_point']
+    return re.sub(r'[^0-9' + decimal_point_char + r']+', '', price)
+
+
+def extract_address(response):
+    """
+
+    :param response:
+    :return:
+    """
+    address_option = response.css("span.postingtitletext small::text").extract_first()
+    # Address are formatted as " (ADDRESS)"
+    if address_option is None:
+        return ""
+    else:
+        return address_option.lstrip(" (").rstrip(' )')
+
+
+def extract_bdrs_bths_area(response):
+    """
+
+    :param response:
+    :return:
+    """
+    raw_bdrs_bths_area = response.css("p.attrgroup span.shared-line-bubble b::text").extract()
+    bdrs_bths_area = " ".join(raw_bdrs_bths_area).lower()
+
+    area = extract_area(bdrs_bths_area)
+    num_bathrooms = extract_bathrooms(bdrs_bths_area)
+    num_bedrooms = extract_bedrooms(bdrs_bths_area)
+    return (num_bedrooms, num_bathrooms, area)
+
+
+def extract_housing_type(attributes):
+    """
+    Return the type of housing if available.
+
+    :param attributes:
+    :return:
+    """
+    return "TODO"
+
+
+def extract_laundry_type(attributes):
+    """
+    Return the type of laundry.
+
+    :param attributes:
+    :return:
+    """
+    return False
+
+
+def extract_parking_type(attributes):
+    """
+    Return the type of parking if available.
+
+    :param attributes:
+    :return:
+    """
+    return 0
