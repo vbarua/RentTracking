1da570420924d5816424494b8850b254aacb99d0
Victor Barua <victor.barua@gmail.com>
2018-10-12 12:43:14 -0700

    Documenting Craiglist parser and output

--- README.md
+++ README.md
@@ -14,6 +14,41 @@ scrapy crawl Craigslist -o RentTrackers/output/Craigslist/results/output-craigls
 This command runs the Craigslist scraper to extract rental data.
 The `RentTrackers/output` directory is ignored by version control
 
+#### Output Schema
+
+```
+Format
+    field_name: type {set of possible values if finite}
+
+The Option type indicates that a field may not be present in the output.
+
+{
+    post_id:        Integer,
+    post_time:      Timestamp,
+    url:            String,
+    latitude:       Option[String],
+    longitude:      Option[String],
+    price:          Option[Integer],
+    address:        Option[String],
+    area:           Option[Integer],
+    bathrooms:      Option[String]      {"1", "1.5", "2", ..., "8.5", "9+", "shared", "split"},
+    bedrooms:       Option[Integer]     {1, 2, ..., 8},
+    is_no_smoking:  Option[Boolean],
+    is_furnished:   Option[Boolean],
+    dogs_allowed:   Option[Boolean],
+    cats_allowed:   Option[Boolean],
+    housing_type:   Option[String]
+        {"apartment", "condo", "cottage/cabin", "duplex", "flat", "house", "in-law",
+         "loft", "townhouse", "manufactured", "land", "assisted living"},
+    laundry_type:   Option[String]
+        {"w/d in unit", "w/d hookups", "laundry in bldg", "laundry on site", "no laundry on site"},
+    parking_type:   Option[String]
+        {"carport", "attached garage", "detached garage", "off-street parking",
+         "street parking", "valet parking", "no parking"},
+    is_wheelchair_accessible: Option[Boolean],
+}
+```
+
 ### TODO
 - Implement page caching to avoid making unnecessary requests to Craigslist.
 - Implement date cutoff to only scrape new postings.
--- RentTrackers/spiders/CraigslistParsingUtilities.py
+++ RentTrackers/spiders/CraigslistParsingUtilities.py
@@ -1,43 +1,39 @@
-import locale
 import re
 
 
-def extract_area(s):
+def extract_area(s) -> dict:
     """
-    Extract area from string like: '1br 1ba 600'. Last number is area.
-
+    Extracts area from a string like: '1br 1ba 600'. Last number is area. May not be present.
     :param s: the string to parse
-    :return: an extraction of the square footage, if found
+    :return: dict containing an area field if present.
     """
     match = re.search(r'\d+$', s)
     if match:
         area = match.group()
-        return {"area": area}
+        return {"area": int(area)}
     else:
         return {}
 
 
 def extract_bedrooms(s) -> dict:
     """
-    Extract # of bathrooms from a string like: '1br 1ba 600'
-
+    Extracts # of bedrooms from a string like: '1br 1ba 600'. May not be present.
     :param s: the string to parse
-    :return: an extraction of the number of bedrooms, if found
+    :return: dict containing a bedrooms field if present.
      """
     match = re.search(r'\d+br', s)
     if match:
         bedrooms = match.group()[:-2]
-        return {"bedrooms": bedrooms}
+        return {"bedrooms": int(bedrooms)}
     else:
         return {}
 
 
 def extract_bathrooms(s) -> dict:
     """
-    Extract # of bedrooms from a string like: '1br 1ba 600', '1br sharedba 400'
-
+    Extracts bathroom type from a string like: '1br 1ba 600' or '1br splitba 600'. May not be present.
     :param s: the string to parse
-    :return: an extraction of the number of bathrooms, if found
+    :return: dict containing a bathrooms field if present.
     """
     num_match = re.search(r'\d(\.\d)?ba', s)
     if num_match:
@@ -52,10 +48,9 @@ def extract_bathrooms(s) -> dict:
 
 def extract_price(response) -> dict:
     """
-    Extract price from string like '$1000'. Prices are always integers but are not always present.
-
+    Extracts price from a string like '$1000'. Prices are always integers. May not be present.
     :param response: a scrapy.http.response.Response
-    :return: dictionary containing a price field if present.
+    :return: dict containing a price field if present.
     """
     price = response.css('span.price::text').extract_first()
     if price:
@@ -67,12 +62,12 @@ def extract_price(response) -> dict:
 
 def extract_address(response) -> dict:
     """
-
-    :param response:
-    :return:
+    Extracts address string from a response. May not be present.
+    :param response: a scrapy.http.response.Response
+    :return: dict containing an address field if present.
     """
     address_option = response.css("span.postingtitletext small::text").extract_first()
-    # Address are formatted as " (ADDRESS)"
+    # Addresses are formatted as " (ADDRESS)"
     if address_option is None:
         return {}
     else:
@@ -80,11 +75,11 @@ def extract_address(response) -> dict:
         return {"address": clean_address}
 
 
-def extract_bdrs_bths_area(response: dict):
+def extract_bdrs_bths_area(response) -> dict:
     """
-
-    :param response:
-    :return:
+    Extracts bedrooms, bathrooms and area values from a response if they are present.
+    :param response: a scrapy.http.response.Response
+    :return: dict containing bedrooms, bathrooms and area fields if they are present.
     """
     raw_bdrs_bths_area = response.css("p.attrgroup span.shared-line-bubble b::text").extract()
     bdrs_bths_area = " ".join(raw_bdrs_bths_area).lower()
@@ -97,6 +92,11 @@ def extract_bdrs_bths_area(response: dict):
 
 
 def extract_lat_long(response) -> dict:
+    """
+    Extracts latitude and longitude values.
+    :param response: a scrapy.http.response.Response
+    :return: dict containing latitude and longitude fields.
+    """
     latitude = response.css("#map::attr(data-latitude)").extract_first()
     longitude = response.css("#map::attr(data-longitude)").extract_first()
     return {
@@ -106,6 +106,11 @@ def extract_lat_long(response) -> dict:
 
 
 def extract_attributes(response) -> dict:
+    """
+    Extract attributes from a Craigslist listing.
+    :param response: a scrapy.http.response.response
+    :return: dict containing data from a Craigslist listing.
+    """
     results = {}
     results.update(extract_price(response))
     results.update(extract_address(response))
@@ -150,7 +155,12 @@ VALID_PARKING_TYPES = {
 }
 
 
-def extract_unstructured_attributes(response):
+def extract_unstructured_attributes(response) -> dict:
+    """
+    Extract values stored in attrgroup tags from a Craigslist listing.
+    :param response: a scrapy.http.response.response
+    :return: dict containing unstructured attribute data.
+    """
     attrs = response.css("p.attrgroup span::text").extract()
     attrs = [a.lower() for a in attrs]
     results = {}
