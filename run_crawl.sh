4f0bda8f53740d7d82da6f91df2a5f4bc2566ae4
Victor Barua <victor.barua@gmail.com>
2018-10-12 14:29:21 -0700

    Updating crawling script

--- run_crawl.sh
+++ run_crawl.sh
@@ -1,7 +1,15 @@
 #!/usr/bin/env bash
+set -e
 
 now=`date +%Y-%m-%d.%H:%M:%S`
+mkdir output/${now}
 export CITY=vancouver
-export CRAWL_SET_LOCATION=output/crawl-set-$now.json
 
-scrapy crawl -set=DOWNLOAD_DELAY=1 -o ${CRAWL_SET_LOCATION} CraigslistSearch
+export CL_CRAWL_SET_LOCATION=output/${now}/cl-crawl-set.json
+
+CL_CRAWL_OUTPUT_LOCATION=output/${now}/cl-output.json
+cl_search_logfile=output/${now}/cl_search_log.txt
+cl_listing_logfile=output/${now}/cl_listing_log.txt
+
+scrapy crawl --set=DOWNLOAD_DELAY=0.25 --logfile=${cl_search_logfile}  --loglevel=INFO -o ${CL_CRAWL_SET_LOCATION} CraigslistSearch
+scrapy crawl --set=DOWNLOAD_DELAY=2 --logfile=${cl_listing_logfile} --loglevel=INFO -o ${CL_CRAWL_OUTPUT_LOCATION} CraigslistListings
