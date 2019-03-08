65e77fa3dfe4b288044b0486f50b68596294f543
Victor Barua <victor.barua@gmail.com>
2019-03-07 19:10:29 -0800

    Updating output target to city/year/month/day/time/

--- run_crawl.sh
+++ run_crawl.sh
@@ -1,15 +1,23 @@
 #!/usr/bin/env bash
 set -e
 
-now=`date +%Y-%m-%d.%H:%M:%S`
-mkdir output/${now}
-export CITY=vancouver
+for city in vancouver
+do
+    export CITY=${city}
+    NOW=`date +%Y/%m/%d/%H%M%S`
+    OUTPUT_LOC=output/${CITY}/${NOW}
+    mkdir -p ${OUTPUT_LOC}
 
-export CL_CRAWL_SET_LOCATION=output/${now}/cl_crawl_set.json
+    #Stores listings that will be scraped in crawl. Written by CraigslistSearch, read by CraigslistListings.
+    export CL_CRAWL_SET_LOCATION=${OUTPUT_LOC}/cl_crawl_set.json
 
-CL_CRAWL_OUTPUT_LOCATION=output/${now}/cl_output.json
-cl_search_logfile=output/${now}/cl_search_log.txt
-cl_listing_logfile=output/${now}/cl_listing_log.txt
+    #Stores output of listings crawl.
+    CL_CRAWL_OUTPUT_LOCATION=${OUTPUT_LOC}/cl_output.json
 
-scrapy crawl --set=DOWNLOAD_DELAY=0.25 --logfile=${cl_search_logfile}  --loglevel=INFO -o ${CL_CRAWL_SET_LOCATION} CraigslistSearch
-scrapy crawl --set=DOWNLOAD_DELAY=2 --logfile=${cl_listing_logfile} --loglevel=INFO -o ${CL_CRAWL_OUTPUT_LOCATION} CraigslistListings
+    #Logfiles
+    CL_SEARCH_LOGFILE=${OUTPUT_LOC}/cl_search_log.txt
+    CL_LISTING_LOGFILE=${OUTPUT_LOC}/cl_listing_log.txt
+
+    scrapy crawl --set=DOWNLOAD_DELAY=0.25 --logfile=${CL_SEARCH_LOGFILE}  --loglevel=INFO -o ${CL_CRAWL_SET_LOCATION} CraigslistSearch
+    scrapy crawl --set=DOWNLOAD_DELAY=2 --logfile=${CL_LISTING_LOGFILE} --loglevel=INFO -o ${CL_CRAWL_OUTPUT_LOCATION} CraigslistListings
+done
