3a16cc284d450d31e2cb66c3ecf41554ba99ac43
Victor Barua <victor.barua@gmail.com>
2019-03-08 16:32:24 -0800

    Update test script with new environment variables

--- test.sh
+++ test.sh
@@ -2,6 +2,11 @@
 now=`date +%Y-%m-%d.%H:%M:%S`
 
 export TEST=true
+export CITY=test
 export CL_CRAWL_SET_LOCATION=dummy
+
+OUTPUT_LOC=test_output
+rm -rf ${OUTPUT_LOC}
+export CL_HTML_OUTPUT_LOCATION=${OUTPUT_LOC}/html
 
 scrapy crawl --set=DOWNLOAD_DELAY=0 CraigslistListings
