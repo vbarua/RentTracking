d617ddb8086a74aec300fa98be503c153940a9fe
Victor Barua <victor.barua@gmail.com>
2019-08-18 15:03:59 -0700

    Update test.sh to be more like actual crawl

--- test.sh
+++ test.sh
@@ -1,12 +1,18 @@
 #!/usr/bin/env bash
-now=`date +%Y-%m-%d.%H:%M:%S`
 
-export TEST=true
-export CITY=test
+# Unused But Required Exports
 export CL_CRAWL_SET_LOCATION=dummy
 
-OUTPUT_LOC=test_output
-rm -rf ${OUTPUT_LOC}
+# Test Setting
+export TEST=true
+export CITY=test
+
+NOW=`date +%Y/%m/%d/%H%M%S`
+OUTPUT_LOC=output/${CITY}/${NOW}
+CL_CRAWL_OUTPUT_LOCATION=${OUTPUT_LOC}/cl_output.json
 export CL_HTML_OUTPUT_LOCATION=${OUTPUT_LOC}/html
 
-scrapy crawl --set=DOWNLOAD_DELAY=0 CraigslistListings
+# Test CraigslistListings Crawler
+scrapy crawl --set=DOWNLOAD_DELAY=0 CraigslistListings -o ${CL_CRAWL_OUTPUT_LOCATION}
+tar -zcf ${CL_HTML_OUTPUT_LOCATION}.tar.gz ${CL_HTML_OUTPUT_LOCATION}
+rm -rf ${CL_HTML_OUTPUT_LOCATION}
